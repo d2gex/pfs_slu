7a7807441245d6023d6be7d75fc2d314dfeb0b3f
d2gex <daniel.garcia@d2garcia.com>
2022-12-20 11:33:19 +0100

    Move global variables needed to config.py

--- python/config.py
+++ python/config.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from os import path
+from os import path
+
 file_path = Path(__file__).resolve()
 ROOT_PATH = str(file_path.parents[1])
 DATA_PATH = path.join(ROOT_PATH, 'data')
+
+sprat_data_path = path.join(DATA_PATH, 'project', 'VPA_assignment_sprat.xlsx')
